dd17146efa8fd189052a64666bf8337d6dcb0af2
mj756 <82023102+mj756@users.noreply.github.com>
2022-08-21 18:58:48 +0530

    getting permission using method channel
    added method channel functionality to test getting permission using method channel

--- android/app/src/main/kotlin/com/flutter/sample/MainActivity.kt
+++ android/app/src/main/kotlin/com/flutter/sample/MainActivity.kt
@@ -1,6 +1,51 @@
 package com.flutter.sample
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Bundle
+import androidx.annotation.NonNull
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
 
 class MainActivity: FlutterActivity() {
+    private val CHANNEL = "samples.flutter.dev/permission"
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+       // GeneratedPluginRegistrant.registerWith(flutterEngine);
+        MethodChannel(
+                flutterEngine.dartExecutor.binaryMessenger,
+                CHANNEL
+        ).setMethodCallHandler { call, result ->
+            if (call.method == "permission") {
+
+               var res=checkPermission(Manifest.permission.CAMERA,
+                        101);
+                result.success(res)
+            } else {
+                result.notImplemented()
+            }
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+    private fun checkPermission(permission: String, requestCode: Int):Int {
+
+        if (ContextCompat.checkSelfPermission(this@MainActivity, permission) == PackageManager.PERMISSION_DENIED) {
+            // Requesting the permission
+             ActivityCompat.requestPermissions(this@MainActivity, arrayOf(permission), requestCode)
+            if(ContextCompat.checkSelfPermission(this@MainActivity, permission)== PackageManager.PERMISSION_DENIED){
+                return -2;} else {
+                return 0;
+            }
+        }else
+        {
+            return 0;
+        }
+        return -1;
+    }
 }
--- lib/view/home.dart
+++ lib/view/home.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_sample/widget/drawer.dart';
 class HomePage extends StatelessWidget
 {
+  static const platform = MethodChannel('samples.flutter.dev/permission');
   const HomePage({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
@@ -11,7 +13,17 @@ class HomePage extends StatelessWidget
        title: const Text('Home'),
      ),
      drawer: const SideBar(),
-     body: Center(child: Text(AppLocalizations.of(context)!.home)),
+     body: Column(
+         children:[
+           Center(child: Text(AppLocalizations.of(context)!.home)),
+          ElevatedButton(onPressed: ()async{
+
+           final result= await platform.invokeMethod<int>('permission');
+           print(result);
+
+          }, child: const Text('Check permission'))
+    ]
+    ),
    );
   }
 
